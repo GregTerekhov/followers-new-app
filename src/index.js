31ea8d4572a464e3b9268f0e0cdf274a10a09f9b
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-06-04 17:59:41 +0300

    fix path to open server

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/test-followers-app">
+      <BrowserRouter basename="/followers-new-app">
         <App />
       </BrowserRouter>
     </PersistGate>
