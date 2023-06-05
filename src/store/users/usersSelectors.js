19828c5d81957d8840a373c553c19351ae2748ba
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-06-05 18:09:14 +0300

    fix

--- src/store/users/usersSelectors.js
+++ src/store/users/usersSelectors.js
@@ -1,21 +1,3 @@
 export const selectUsers = state => state.users.items;
 export const selectPage = state => state.users.page;
 export const selectFilter = state => state.users.filter;
-// export const selectFilteredUsers = state => {
-//   const filter = state.filter;
-//   const items = state.items;
-
-//   if (filter === 'showAll') {
-//     return items;
-//   }
-
-//   if (filter === 'Follow') {
-//     return items.filter(item => !state.followersIds.includes(item.id));
-//   }
-
-//   if (filter === 'Following') {
-//     return items.filter(item => state.followersIds.includes(item.id));
-//   }
-
-//   return items;
-// };
